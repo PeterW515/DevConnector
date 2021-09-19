0524e9feffe5fa6c7f2a1c07e990b6163613e864
Peter Weigman <phweigman@gmail.com>
2021-09-18 22:18:44 -0400

    user model

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
 
+// Define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/post'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
